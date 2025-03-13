a90c85a5304f93f860cca8c09798ff38d18b0249
Sumeet bhagat <sumeetbhagat1811@gmail.com>
2025-03-13 23:27:48 +0530

    Changed DB connection

--- backend/api/config/mongodbconn.js
+++ backend/api/config/mongodbconn.js
@@ -4,13 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Use environment variable for MongoDB URI
-// const uri = process.env.MONGO_URI;
-const uri = 'mongodb+srv://sumeetbhagat469:sumeetbhagat469@cluster0.0dqqd.mongodb.net/loginDetails';
+const uri = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(uri);
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(`✅ Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ Error: ${error.message}`);
     process.exit(1);
